883701190e973ca549cee12a01aca38d06229f26
Paul D Freeman <paul@pdfreeman.com>
2016-09-17 23:51:05 +0100

    Initial Creation of SchoolBus

---
+++ SchoolBus/SchoolBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SchoolBus
+//
+//  Created by Paul Freeman on 17/09/2016.
+//  Copyright © 2016 Paul Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
