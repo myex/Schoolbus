fe5ebc36b7ffa9839d99f4826b06e94197cee8b9
Paul D Freeman <paul@pdfreeman.com>
2016-10-31 08:04:36 +0000

    push up

--- SchoolBus/SchoolBus/ViewController.swift
+++ SchoolBus/SchoolBus/ViewController.swift
@@ -18,22 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnStop: UIButton!
     @IBOutlet weak var txtLog: UITextView!
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("hello there")
-        var r = Location.getLocation(withAccuracy: .any, frequency: .oneShot, timeout: nil, onSuccess: { (loc) in
-            print("loc \(loc)")
-        }) { (last, err) in
-            print("err \(err)")
-        }
-        r.onAuthorizationDidChange = { newStatus in
-            print("New status \(newStatus)")
-        }
-        print("done!!!!")
-    }
-        
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -44,7 +29,19 @@ class ViewController: UIViewController {
     
     @IBAction func btnStartClicked(_ sender: UIButton) {
         txtLog.text = "Start locating\n" + txtLog.text
+
+        debugPrint("hello there")
+        var r = Location.getLocation(withAccuracy: .any, frequency: .oneShot, timeout: nil, onSuccess: { (loc) in
+            debugPrint("loc \(loc)")
+        }) { (last, err) in
+            print("err \(err)")
+        }
+        r.onAuthorizationDidChange = { newStatus in
+            debugPrint("New status \(newStatus)")
+        }
+        debugPrint("done!!!!")
     }
+
 
     @IBAction func btnStopClicked(_ sender: UIButton) {
         txtLog.text = "Stopping locating\n" + txtLog.text
